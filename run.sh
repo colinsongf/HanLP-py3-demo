4fc9deec86f09ccc2d20526cc2cf709875eef7a8
alex cai <caiyingyao@ibbd.net>
2017-04-18 16:38:08 +0800

    run

--- run.sh
+++ run.sh
@@ -4,11 +4,13 @@
 # Author: Alex
 # Created Time: 2017年04月16日 星期日 16时30分21秒
 
+container=ibbd/java-python3
+
 # 先删除旧
-docker rm ibbd-java-py3
+docker rm $container
 
 # 重新运行
-docker run -ti --name=ibbd-java-py3 \
+docker run -ti --name=ibbd-java-py3-server \
     -v /var/www/hanlp/:/var/hanlp/ \
     -v /var/www/:/var/www/ \
-    ibbd/java-python3 /bin/bash
+    $container /bin/bash
